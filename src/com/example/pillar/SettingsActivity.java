e15ef02bd17917ab730cabe54408d0913f6d48be
Arindra Kumar Das <arindra.das@live.com>
2014-03-23 21:19:08 +0100

    Bluetooth Status checking on start

--- src/com/example/pillar/SettingsActivity.java
+++ src/com/example/pillar/SettingsActivity.java
@@ -1,36 +1,105 @@
 package com.example.pillar;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Calendar;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 
 	private TextView tvDisplayTime;
 	private Button btnChangeTime;
-
 	private int hour;
 	private int minute;
 
 	static final int TIME_DIALOG_ID = 999;
 
+	
+	private static final int REQUEST_ENABLE_BT = 1;
+	  private BluetoothAdapter btAdapter = null;
+	  private BluetoothSocket btSocket = null;
+	  private OutputStream outStream = null;
+	   
+	  // Well known SPP UUID
+	  private static final UUID MY_UUID =
+	      UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	 
+	  // Insert your server's MAC address
+	  private static String address = "00:00:00:00:00:00";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//Log.d(TAG, "In onCreate()");
 		setContentView(R.layout.settings_view);
 
 		setCurrentTimeOnView();
-		addListenerOnButton();
+		addListenerOnButton(); 
+		    btAdapter = BluetoothAdapter.getDefaultAdapter();
+		    checkBTState();
+		 
+		    
+		  }
+		   
+		  private void checkBTState() {
+		    // Check for Bluetooth support and then check to make sure it is turned on
+		 
+		    // Emulator doesn't support Bluetooth and will return null
+		    if(btAdapter==null) { 
+		      errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
+		    } else {
+		      if (btAdapter.isEnabled()) {
+		      //  Log.d(TAG, "...Bluetooth is enabled...");
+		      } else {
+		        //Prompt user to turn on Bluetooth
+		        Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+		        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		      }
+		    }
+		  }
+		 
+		  private void errorExit(String title, String message){
+		    Toast msg = Toast.makeText(getBaseContext(),
+		        title + " - " + message, Toast.LENGTH_SHORT);
+		    msg.show();
+		    finish();
+		  }
+		 
+		  private void sendData(String message) {
+		    byte[] msgBuffer = message.getBytes();
+		 
+		  //  Log.d(TAG, "...Sending data: " + message + "...");
+		 
+		    try {
+		      outStream.write(msgBuffer);
+		    } catch (IOException e) {
+		      String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+		      if (address.equals("00:00:00:00:00:00")) 
+		        msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
+		      msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+		       
+		      errorExit("Fatal Error", msg);       
+		    }
+		  }
 
-	}
 
+	
+	
+	
+	
 	// display current time
 	public void setCurrentTimeOnView() {
 
@@ -95,4 +164,7 @@ public class SettingsActivity extends Activity {
 		else
 			return "0" + String.valueOf(c);
 	}
+
+
+
 }
