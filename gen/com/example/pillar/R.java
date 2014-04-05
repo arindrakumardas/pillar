5e6b2e54e487d21ce1e9e56a1ef0854070620f68
Arindra Kumar Das <arindra.das@live.com>
2014-04-05 12:40:52 +0200

    New codes added

--- gen/com/example/pillar/R.java
+++ gen/com/example/pillar/R.java
@@ -21,20 +21,37 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int edit_icon=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bluetooth=0x7f020000;
+        public static final int bluetooth_disconnected=0x7f020001;
+        public static final int edit_icon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int info=0x7f020004;
+        public static final int product=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int btnChangeTime=0x7f080001;
+        public static final int TextView1=0x7f080002;
+        public static final int TextView2=0x7f080004;
+        public static final int TextView3=0x7f080005;
+        public static final int action_settings=0x7f08000f;
+        public static final int bluetooth=0x7f080009;
+        public static final int btnChangeTime=0x7f08000a;
+        public static final int editText1=0x7f080007;
+        public static final int editText3=0x7f080003;
+        public static final int editText4=0x7f080006;
+        public static final int info=0x7f08000e;
         public static final int load_view=0x7f080000;
-        public static final int set_button=0x7f080003;
-        public static final int tvTime=0x7f080002;
+        public static final int logo=0x7f080001;
+        public static final int product=0x7f080008;
+        public static final int set_button=0x7f08000c;
+        public static final int textView1=0x7f08000d;
+        public static final int tvTime=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int connection_view=0x7f030001;
-        public static final int settings_view=0x7f030002;
+        public static final int authenticate_view=0x7f030001;
+        public static final int connection_view=0x7f030002;
+        public static final int instruction_view=0x7f030003;
+        public static final int settings_view=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -44,7 +61,11 @@ public final class R {
         public static final int Set=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int email=0x7f050007;
         public static final int hello_world=0x7f050002;
+        public static final int logo=0x7f050005;
+        public static final int name=0x7f050006;
+        public static final int pillar=0x7f050008;
     }
     public static final class style {
         /** 
---
+++ src/com/example/pillar/InstructionActivity.java
@@ -0,0 +1,16 @@
+package com.example.pillar;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+
+public class InstructionActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+}}
